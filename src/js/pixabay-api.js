f01d27054b497fb7934c7514196c6ddca05b68ae
zalevskiy <zaleva100@gmail.com>
2025-12-07 19:09:57 +0200

    add js #12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,25 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 
 const API_KEY = '53408779-ee8ac1d107b84f4a01a68d938';
 const PER_PAGE = 15;
 
 export async function getImagesByQuery(query, page = 1) {
-  const url = new URL(BASE_URL);
-  url.searchParams.set('key', API_KEY);
-  url.searchParams.set('q', query.trim());
-  url.searchParams.set('image_type', 'photo');
-  url.searchParams.set('orientation', 'horizontal');
-  url.searchParams.set('safesearch', 'true');
-  url.searchParams.set('per_page', String(PER_PAGE));
-  url.searchParams.set('page', String(page));
+  const params = {
+    key: API_KEY,
+    q: query.trim(),
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: PER_PAGE,
+    page: page,
+  };
 
   try {
-    const res = await fetch(url.toString());
-    if (!res.ok) {
-      throw new Error(`Pixabay API error: ${res.status} ${res.statusText}`);
-    }
-    const data = await res.json();
+    const response = await axios.get(BASE_URL, { params });
 
-    return data;
+    return response.data;
   } catch (error) {
     throw error;
   }
